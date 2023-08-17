ae4d62db162fdac7fc282034ebcad0c2993a46a1
Arthur <a-murk@mail.ee>
2023-08-17 12:37:19 +0300

    Create get data logic for API service

--- src/app/api-content.service.ts
+++ src/app/api-content.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ApiContentService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getData() {
+    return this.http.get('https://services.err.ee/api/v2/category/getByUrl?url=video&domain=jupiter.err.ee')
+  }
 }
