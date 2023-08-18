5f25c90c35c8c8ce230d913e414811c44830a34e
Arthur <a-murk@mail.ee>
2023-08-18 19:19:10 +0300

    Add logic for handling scrolling, update button width

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { ApiContentService } from '../api-content.service';
 
 @Component({
@@ -6,23 +6,42 @@ import { ApiContentService } from '../api-content.service';
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss']
 })
-
-export class SliderComponent implements OnInit {
+export class SliderComponent {
   categories: any[] = [];
+  incrementValues: { [key: number]: number } = {};
 
-  constructor (private apiCall: ApiContentService) {}
+  constructor(private apiCall: ApiContentService, private renderer: Renderer2) {}
 
   ngOnInit(): void {
-      this.getCategories();
-  }
-
-  getCategories(): void {
     this.apiCall.getData().subscribe({
       next: (res) => {
         this.categories = res.data.category.frontPage;
+        this.categories.forEach((category, index) => {
+          this.incrementValues[index] = 0;
+        });
       }
-    })
+    });
+  }
+
+ moveLeft(categoryIndex: number): void {
+  if (this.incrementValues[categoryIndex] > 0) {
+    this.incrementValues[categoryIndex]--;
+    this.updateSliderTransform(categoryIndex);
+  }
+}
+
+moveRight(categoryIndex: number): void {
+  const category = this.categories[categoryIndex];
+  if (this.incrementValues[categoryIndex] < category.data.length - 1) {
+    this.incrementValues[categoryIndex]++;
+    this.updateSliderTransform(categoryIndex);
   }
 }
 
 
+  private updateSliderTransform(categoryIndex: number): void {
+    const transformValue = `translateX(${ -100 * this.incrementValues[categoryIndex] }%)`;
+    const sliderElement = document.getElementById(`slider-${categoryIndex}`);
+    this.renderer.setStyle(sliderElement, 'transform', transformValue);
+  }
+}
